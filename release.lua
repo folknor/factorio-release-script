4d26781e1f0cc429d8f5247255b2ae3dfe60d1fd
Vidar Haarr <vhaarr@gmail.com>
2017-08-11 18:39:37 +0200

    Add version check on launch.

--- release.lua
+++ release.lua
@@ -1,5 +1,5 @@
 #!/usr/bin/lua
-local VERSION = 1
+local VERSION = 2
 -- lua5.2 release script for factorio mods
 -- by folk@folk.wtf
 -- Requires https://github.com/zserge/luash and https://github.com/whiteinge/ok.sh
@@ -8,13 +8,27 @@ local VERSION = 1
 -- LICENSE CC-BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0
 
 local sh = require("sh")
+local _ = tostring
+local source = _(curl("-s", "https://raw.githubusercontent.com/folknor/factorio-release-script/master/release.lua"))
+if type(source) == "string" then
+	local currentVersion = tonumber(source:match("local VERSION = (%d+)"))
+	if type(currentVersion) == "number" and currentVersion > VERSION then
+		local answer
+		repeat
+			io.write("There is a new version of factorio-release-script available, do you want to exit (y/n)? ")
+			io.flush()
+			answer = io.read()
+		until answer == "y" or answer == "n"
+		if answer == "y" then return end
+	end
+end
+
 local oksh = sh.command("ok.sh")
 local function exit(...) io.write(..., "\n") os.exit() end
 
 local maj, min, patch = tonumber((select(1, ...))), tonumber((select(2, ...))), tonumber((select(3, ...)))
 if type(maj) ~= "number" or type(min) ~= "number" or type(patch) ~= "number" then exit("Please provide an additive version number bump, like 0 0 10, 0 1 0, 1 0 2, or 0 0 0 for no bump.") end
 
-local _ = tostring
 local mod = _(cat("info.json"))
 local name = mod:match("\"name\":%s+\"(%S+)\"")
 if type(name) ~= "string" or name:len() < 5 then exit("Could not parse name from info.json.") end
